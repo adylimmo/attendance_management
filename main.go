f01f39bdc891f8bf0713d984f2d28061c1bf4aaf
ady suryadi <ady.limmo@gmail.com>
2023-11-30 09:23:00 +0700

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 	"github.com/raihaninfo/attendance_magagment/controllers"
 	"github.com/raihaninfo/attendance_magagment/db"
 	_ "github.com/raihaninfo/attendance_magagment/docs"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	
 )
 
 var Port string = ":3000"
@@ -16,10 +15,5 @@ func main() {
 	DB := db.Init()
 	fmt.Println("Listing to port : ", Port)
 	controllers.Controller(Port, DB)
-	router := server.Group("/api")
-	router.GET("/healthchecker", func(ctx *gin.Context) {
-		message := "Welcome to Golang with Gorm and Postgres"
-		user := "Welcome to Golang with Gorm and Postgres"
-		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message, "user": user})
-	})
+	
 }
