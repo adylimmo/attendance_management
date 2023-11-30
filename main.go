6747b29f1c39df8993a7b149329851327b930bd6
ady suryadi <ady.limmo@gmail.com>
2023-11-30 09:18:05 +0700

    Update main.go

--- main.go
+++ main.go
@@ -14,4 +14,10 @@ func main() {
 	DB := db.Init()
 	fmt.Println("Listing to port : ", Port)
 	controllers.Controller(Port, DB)
+	router := server.Group("/api")
+	router.GET("/healthchecker", func(ctx *gin.Context) {
+		message := "Welcome to Golang with Gorm and Postgres"
+		user := "Welcome to Golang with Gorm and Postgres"
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message, "user": user})
+	})
 }
